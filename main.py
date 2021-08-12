523360d418136b12a0bd29b9dd168f63c9d4b12e
Axel <axel.zenine@epitech.eu>
2021-08-12 02:58:32 +0200

    [ADD] new screens

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ pygame.init()
 clock=pygame.time.Clock()
 cp=pygame.mixer.Sound("ressources/case pleine.wav")
 
-fenetre=pygame.display.set_mode((639,479))
+fenetre=pygame.display.set_mode((800,600))
 
-rouge = pygame.image.load("ressources/P4-rouge.png")
+rouge = pygame.image.load("ressources/RedDisc.png")
 rougerect=rouge.get_rect()
 rougerectinit=rouge.get_rect()
-bleu = pygame.image.load("ressources/P4-bleu.png")
-bleurect=bleu.get_rect()
-bleurectinit=bleu.get_rect()
+Yellow = pygame.image.load("ressources/YellowDisc.png")
+Yellowrect=Yellow.get_rect()
+Yellowrectinit=Yellow.get_rect()
 fondnoir= pygame.image.load("ressources/fondnoir.png")
 fondecran=pygame.image.load("ressources/fondecran.png")
 rougewin=pygame.image.load("ressources/rougewin.png")
 jaunewin=pygame.image.load("ressources/jaunewin.png")
-menuim= pygame.image.load("ressources/menuimage.png")
+menuim= pygame.image.load("ressources/Title_Screen.png")
 yrouge=6
 
 
@@ -34,18 +34,18 @@ def menu():
         for event in pygame.event.get():
             if event.type==QUIT:
                 continuer == False
-            if event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[1]<355 and  event.pos[1] > 314 and event.pos[0]<162 and  event.pos[0] > 78:
+            if event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[1]< 395 and  event.pos[1] > 340 and event.pos[0] < 550 and  event.pos[0] > 250:
                 commencer=True
                 continuer=False
                 choix=1
-            if event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[1]<419 and  event.pos[1] > 380 and event.pos[0]<420 and  event.pos[0] > 233:
+            if event.type == MOUSEBUTTONUP and event.button == 1 and event.pos[1]< 521 and  event.pos[1] > 468 and event.pos[0] < 550 and  event.pos[0] > 250:
                 choix = 2
                 commencer = False
                 continuer=False
 
     return commencer,choix
 
-def bougejeton(joueur,x,y,position,rougerect,bleurect):
+def bougejeton(joueur,x,y,position,rougerect,Yellowrect):
     global taby,tabx
     if joueur==1 :
         rougerect=rougerect.move(x,y)
@@ -59,16 +59,16 @@ def bougejeton(joueur,x,y,position,rougerect,bleurect):
             pygame.display.flip()
             salejetonr=fondecran.subsurface(rougerect)
     if joueur==2 :
-        bleurect=bleurect.move(x,y)
-        fenetre.blit(bleu,bleurect)
+        Yellowrect=Yellowrect.move(x,y)
+        fenetre.blit(Yellow,Yellowrect)
         pygame.display.flip()
-        salejetonr=fondecran.subsurface(bleurect)
+        salejetonr=fondecran.subsurface(Yellowrect)
         for i in range(taby[position]):
-            fenetre.blit(salejetonr, bleurect)
-            bleurect=bleurect.move(0,1)
-            fenetre.blit(bleu,bleurect)
+            fenetre.blit(salejetonr, Yellowrect)
+            Yellowrect=Yellowrect.move(0,1)
+            fenetre.blit(Yellow,Yellowrect)
             pygame.display.flip()
-            salejetonr=fondecran.subsurface(bleurect)
+            salejetonr=fondecran.subsurface(Yellowrect)
 
 def test(col,couleur):
     li=5
@@ -114,7 +114,7 @@ def verif(grille,couleur):
     return good
 
 
-def tombejeton(joueur,grille):              # joueur 1 --> rouge / 2--> bleu
+def tombejeton(joueur,grille):              # joueur 1 --> rouge / 2--> Yellow
         ok=False
         cont=True
         for event in pygame.event.get():
@@ -124,75 +124,75 @@ def tombejeton(joueur,grille):              # joueur 1 --> rouge / 2--> bleu
                 print ("clic effectué")
                 if event.pos[1]<479:
                     rougerect=rougerectinit
-                    bleurect=bleurectinit
+                    Yellowrect=Yellowrectinit
 
             #première colonne
-                if event.pos[0]<85:
+                if event.pos[0]<142:
                     position=test(0,joueur)
                     if position>-1:
                         grille[0][position]=joueur
                         ok=True
-                        bougejeton(joueur,15,6,position,rougerect,bleurect)
+                        bougejeton(joueur,58, 107,position,rougerect,Yellowrect)
                         print(grille)
 
             #deuxième colonne
 
-                if event.pos[0]<175 and event.pos[0]>85:
+                if event.pos[0]<243 and event.pos[0]>158:
                     position=test(1,joueur)
                     if position>-1:
                         grille[1][position]=joueur
                         ok=True
-                        bougejeton(joueur,105,6,position,rougerect,bleurect)
+                        bougejeton(joueur,158, 107,position,rougerect,Yellowrect)
                         print(grille)
 
             #troisième colonne
 
-                if event.pos[0]<265 and event.pos[0]>175:
+                if event.pos[0]<342 and event.pos[0]>257:
                     position=test(2,joueur)
                     if position>-1:
                         grille[2][position]=joueur
                         ok=True
-                        bougejeton(joueur,195,6,position,rougerect,bleurect)
+                        bougejeton(joueur,257, 107,position,rougerect,Yellowrect)
                         print(grille)
 
             #quatrième colonne
 
-                if event.pos[0]<355 and event.pos[0]>265:
+                if event.pos[0]<444 and event.pos[0]>359:
                     position=test(3,joueur)
                     if position>-1:
                         grille[3][position]=joueur
                         ok=True
-                        bougejeton(joueur,285,6,position,rougerect,bleurect)
+                        bougejeton(joueur,359, 107,position,rougerect,Yellowrect)
                         print(grille)
 
             #cinquième colonne
 
-                if event.pos[0]<445 and event.pos[0]>355:
+                if event.pos[0]<540 and event.pos[0]>458:
                     position=test(4,joueur)
                     if position>-1:
                         grille[4][position]=joueur
                         ok=True
-                        bougejeton(joueur,375,6,position,rougerect,bleurect)
+                        bougejeton(joueur,458, 107,position,rougerect,Yellowrect)
                         print(grille)
 
             #sixième colonne
 
-                if event.pos[0]<535 and event.pos[0]>445:
+                if event.pos[0]<640 and event.pos[0]>557:
                     position=test(5,joueur)
                     if position>-1:
                         grille[5][position]=joueur
                         ok=True
-                        bougejeton(joueur,465,6,position,rougerect,bleurect)
+                        bougejeton(joueur,558,107,position,rougerect,Yellowrect)
                         print(grille)
 
             #septième colonne
 
-                if event.pos[0]>535:
+                if event.pos[0]>658:
                     position=test(6,joueur)
                     if position>-1:
                         grille[6][position]=joueur
                         ok=True
-                        bougejeton(joueur,555,6,position,rougerect,bleurect)
+                        bougejeton(joueur,65, 107,position,rougerect,Yellowrect)
                         print(grille)
 
         ##        cp.play()
@@ -200,8 +200,8 @@ def tombejeton(joueur,grille):              # joueur 1 --> rouge / 2--> bleu
         return cont,ok
         #cont:Ferme la fenêtre ok:Position jeton
 
-tabx=[15,105,195,285,375,465,555,630]
-taby=[-9,79,159,239,319,400]
+tabx=[57,158,195,257,359,458,557,65]
+taby=[-10,0,100,200,300,400]
 commencer,choix=menu()
 #if commencer==False and choix==2:
 
